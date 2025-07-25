fbdc01945d3c15d628286231e53e6764d65a7787
rishabhjha05 <rishabhjha012043@gmail.com>
2025-07-25 14:48:12 +0530

    Time: 15 ms (86.51%) | Memory: 138.1 MB (91.01%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> sol(m,vector<int>(n,-1));
+
+        int minrow=0,mincol=0,maxrow=m-1,maxcol=n-1;
+        ListNode* temp=head;
+        while(minrow<=maxrow && mincol<=maxcol)
+        {
+            for(int j=mincol;j<=maxcol && temp!=NULL ;j++){
+                sol[minrow][j]=temp->val;
+                temp=temp->next;
+            }
+            minrow++;
+            for(int i=minrow;i<=maxrow && temp!=NULL ;i++){
+                sol[i][maxcol]=temp->val;
+                temp=temp->next;
+            }
+            maxcol--;
+            for(int j=maxcol;j>=mincol && temp!=NULL ;j--){
+                sol[maxrow][j]=temp->val;
+                temp=temp->next;
+            }
+            maxrow--;
+            for(int i=maxrow;i>=minrow && temp!=NULL ;i--){
+                sol[i][mincol]=temp->val;
+                temp=temp->next;
+            }
+            mincol++;
+            if(temp==NULL) return sol;
+        }
+        return sol;
+    }
+};
