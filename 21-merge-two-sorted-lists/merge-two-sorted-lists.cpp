5239daed4f12e0dc3768978c51b52b3d9ab3b4a7
rishabhjha05 <rishabhjha012043@gmail.com>
2025-07-25 16:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (11.32%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* temp= new ListNode(-1);
+        ListNode* head=temp;
+        ListNode* temp1=list1,*temp2=list2;
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1->val>temp2->val){
+                ListNode* x=new ListNode(temp2->val);
+                temp->next=x;
+                temp=x;
+                temp2=temp2->next;
+            }
+            else{
+                ListNode* x=new ListNode(temp1->val);
+                temp->next=x;
+                temp=x;
+                temp1=temp1->next;
+            }
+        }
+        if(temp1==NULL)
+            temp->next=temp2;
+        else
+            temp->next=temp1;
+        return head->next;
+        
+    }
+};
