7beaf09b840ce150166c07c33b71a85ee9cbb388
rishabhjha05 <rishabhjha012043@gmail.com>
2025-08-02 01:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (72.68%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -13,24 +13,20 @@ class Solution {
 public:
     Node* flatten(Node* head) {
         Node *temp=head;
-        vector<Node*> Child;
         while(temp){
-            if(temp->next==NULL && Child.size()!=0){
-                temp->next=Child[Child.size()-1];
-                if(Child[Child.size()-1])
-                    Child[Child.size()-1]->prev=temp;
-                Child.pop_back();
-            }
             if(temp->child!=NULL){
-                Child.push_back(temp->next);
-                temp->next=temp->child;
-                temp->child->prev=temp;
+                Node *a=temp->next;
+                Node *c=temp->child;
+                temp->child=NULL;
+                c=flatten(c);
+                temp->next=c;
+                c->prev=temp;
+                while(temp->next)
+                    temp=temp->next;
+                temp->next=a;
+                if(a!=NULL)
+                    a->prev=temp;
             }
-            temp=temp->next;
-        }
-        temp=head;
-        while(temp){
-            temp->child=NULL;
             temp=temp->next;
         }
         return head;
