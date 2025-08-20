9676d20e1ff49759cff7ef5077e82b7d7a337ac6
rishabhjha05 <rishabhjha012043@gmail.com>
2025-08-20 21:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (29.23%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int i=0;
-        while(i<s.size()){
-            if(s[i]!=t[i])
-                return t[i];
-            i++;
-        }
-        return t[i];
+        int i=0,sum=0;
+        while(i<t.size())
+            sum+=(int)t[i++];
+        i=0;
+        while(i<s.size())
+            sum-=(int)s[i++];
+        return (char)sum;
     }
 };
