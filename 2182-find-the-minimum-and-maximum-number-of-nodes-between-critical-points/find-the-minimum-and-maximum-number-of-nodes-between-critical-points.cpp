9bb2536f3c59145d9283b9f2a4469ed5ba64e7ee
rishabhjha05 <rishabhjha012043@gmail.com>
2025-08-25 23:05:04 +0530

    Time: 7 ms (33.42%) | Memory: 128.5 MB (17.61%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,30 +11,40 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head){
-        vector<int> idx;
-        ListNode*temp=head;
-        int i=1;
-        while(temp->next->next){
-            ListNode *prev=temp,*curr=temp->next,*Next=curr->next;
-            if(prev->val>curr->val && curr->val<Next->val)
-                idx.push_back(i);
-            if(prev->val<curr->val && curr->val>Next->val)
-                idx.push_back(i);
-            temp=temp->next;
-            i++;
+        
+    vector<int> idx;
+    int size = 0;
+    ListNode *temp = head;
+    while (temp)
+    {
+        size++;
+        temp = temp->next;
+    }
+    temp = head;
+    for (int i = 1; i < size - 1; i++)
+    {
+        int val = temp->next->val;
+        if (val > temp->val && val > temp->next->next->val)
+            idx.push_back(i);
+        else if (val < temp->val && val < temp->next->next->val)
+            idx.push_back(i);
+        temp=temp->next;
+    }
+    vector<int> ans(2, -1);
+    if (idx.size() < 2)
+        return ans;
+    else
+    {
+        int maxdis = idx[idx.size() - 1] - idx[0];
+        int mindis = INT_MAX;
+        for (int i = 0; i < idx.size() - 1; i++)
+        {
+            int x = idx[i + 1] - idx[i];
+            mindis = min(mindis, x);
         }
-        vector<int> sol(2,-1);
-        if(idx.size()<2)
-            return sol;
-        int n=idx.size(), mn=INT_MAX,max=idx[n-1]-idx[0];
-        i=1;
-        while(i<n){
-            int x=idx[i]-idx[i-1];
-            mn=min(mn,x);
-            i++;
-        }
-        sol[0]=mn;
-        sol[1]=max;
-        return sol;
+        ans[0] = mindis;
+        ans[1] = maxdis;
+    }
+    return ans;
     }
 };
