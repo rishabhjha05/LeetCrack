0090c25b34eafc1909353af508e0d28d14f15f07
rishabhjha05 <rishabhjha012043@gmail.com>
2025-08-28 20:52:07 +0530

    Time: 0 ms (100.00%) | Memory: 124.2 MB (93.86%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -10,17 +10,37 @@
  */
 class Solution {
 public:
-    int pairSum(ListNode* head) {
-        int mx=INT_MIN;
-        vector<int> vec;
-        ListNode* temp=head;
-        while(temp){
-            vec.push_back(temp->val);
-            temp=temp->next;
+    ListNode* rev(ListNode* head){
+        if(head==NULL || head->next==NULL)
+            return head;
+        ListNode* prev=head;
+        ListNode* curr=head->next;
+        ListNode* Next=curr->next;
+        while(Next){
+            curr->next=prev;
+            prev=curr;
+            curr=Next;
+            Next=Next->next;
         }
-        int i=0,j=vec.size()-1;
-        while(i<j)
-            mx=max(mx,vec[i++]+vec[j--]);
+        curr->next=prev;
+        head->next=NULL;
+        return curr;
+    }
+    int pairSum(ListNode* head) {
+        ListNode *slow=head,*fast=head;
+        while(fast->next && fast->next->next){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        ListNode* head2=rev(slow->next);
+        slow->next=NULL;
+        int mx=INT_MIN;
+        ListNode* temp1=head,*temp2=head2;
+        while(temp1 && temp2){
+            mx=max(mx,temp1->val+temp2->val);
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
         return mx;
     }
 };
