56a62c76a322905dbeb5ab5845cd11e108f6b0da
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-04 11:33:44 +0530

    Time: 50 ms (11.78%) | Memory: 89 MB (15.38%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        
-    int n = heights.size(), i = n-1, maxArea = INT_MIN;
-    vector<int> maxBreath(n);
+    int largestRectangleArea(vector<int>& heights) {  
+    int n = heights.size(), i = 0, maxArea = 0;
+    vector<int> prevS(n), nextS(n), maxBreath(n);
     stack<int> st;
-    while(st.size()>0)
-        st.pop();
+    while (i < n)
+    {
+        while (st.size() != 0 && heights[st.top()] >= heights[i])
+            st.pop();
+        if (st.size() == 0)
+            prevS[i] = -1;
+        else
+            prevS[i] = st.top();
+        st.push(i++);
+    }
+    i--;
+    st = stack<int>();
     while (i >= 0)
     {
         while (st.size() != 0 && heights[st.top()] >= heights[i])
             st.pop();
         if (st.size() == 0)
-            maxBreath[i] = n;
+            nextS[i] = n;
         else
-            maxBreath[i] = st.top();
+            nextS[i] = st.top();
         st.push(i--);
-    }
-    i=0;
-    while(st.size()>0)
-        st.pop();
-    while (i < n)
-    {
-        while (st.size() != 0 && heights[st.top()] >= heights[i])
-            st.pop();
-        if (st.size() != 0)
-            maxBreath[i] -= (st.top()+1);
-        st.push(i++);
     }
     i = 0;
     while (i < n)
     {
-        maxArea = max(maxArea,maxBreath[i] * heights[i]);
+        maxBreath[i] = nextS[i] - prevS[i] - 1;
         i++;
     }
+    i = 0;
+    while (i < n)
+        maxArea = max(maxArea, maxBreath[i] * heights[i++]);
     return maxArea;
     }
 };
