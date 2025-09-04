5840a68a2cb89babb4e3a1663556a0c99a8e64bc
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-04 23:31:46 +0530

    Time: 3 ms (98.36%) | Memory: 88.8 MB (28.27%) - LeetSync

---
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        stack<int> st;
+        int n=heights.size(),i=n-1;
+        vector<int> sol(n);
+        while(i>=0){
+            int count=0;
+            while(st.size()!=0 && st.top()<heights[i])
+            {
+                st.pop();
+                count++;
+            }
+            if(st.size()!=0)
+                count++;
+            st.push(heights[i]);
+            sol[i]=count;
+            i--;
+        }
+        return sol;
+    }
+};
