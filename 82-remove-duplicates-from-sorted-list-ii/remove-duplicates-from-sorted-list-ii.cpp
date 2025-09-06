a48ad29289aa3f27d7998457a8a6f56cb3b69fa4
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-07 01:04:39 +0530

    Time: 5 ms (3.10%) | Memory: 15.8 MB (42.18%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -11,19 +11,36 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* dummy=new ListNode(-1);
-        dummy->next=head;
-        ListNode* prev=dummy,*curr=head;
-        while(curr){
-            if(curr->next && curr->val==curr->next->val){
-                while(curr->next && curr->val==curr->next->val)
-                    curr=curr->next;
-                prev->next=curr->next;
-            }
-            else
-                prev=prev->next;
-            curr=curr->next;
+      
+    if (head == NULL || head->next == NULL)
+        return head;
+    ListNode *head2 = new ListNode(-1);
+    ListNode *temp = head, *temp2 = head2;
+    ListNode *prev = head, *curr = prev->next, *Next = curr->next;
+    if (head->val != head->next->val)
+    {
+        cout<<"Yes1";
+        temp2->next = head;
+        temp2=temp2->next;
+    }
+    while (Next)
+    {
+        if (curr->val != prev->val && curr->val != Next->val)
+        {
+            cout<<"Yes";
+            temp2->next = curr;
+            temp2 = temp2->next;
         }
-        return dummy->next;
+        prev = curr;
+        curr = Next;
+        Next = Next->next;
+    }
+    if (prev->val != curr->val)
+    {
+        temp2->next = curr;
+        temp2 = temp2->next;
+    }
+    temp2->next=NULL;
+    return head2->next;
     }
 };
