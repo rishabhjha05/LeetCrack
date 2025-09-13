a2e04bca749774d84488936f5353e7e5f33b9040
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-13 23:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (35.40%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int NoSetbit(int n){
+        int count=0;
+        while(n>0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+    int hammingDistance(int x, int y) {
+        int zor=x^y;
+        cout<<zor;
+        return NoSetbit(zor);
+    }
+};
