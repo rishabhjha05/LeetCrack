34b2abf52bdfa7e33c5ff8ca3b18cae1d5bb1965
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-15 20:35:26 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (8.91%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,44 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* rev(ListNode *head){
-        if(head==NULL || head->next==NULL)
-            return head;
-        ListNode *prev=head,*curr=prev->next,*Next=curr->next;
-        while(Next){
-            curr->next=prev;
-            prev=curr;
-            curr=Next;
-            Next=Next->next;
-            cout<<"3 ";
-        }
-        head->next=NULL;
-        curr->next=prev;
-        return curr;
-    }
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *p=head,*n1=head,*pp=NULL;
-        bool found=false;
-        while(n1){
-            for(int i=1;i<k;i++)
-                if(n1)
-                    n1=n1->next;
-            if(n1){
-                ListNode *n2=n1->next;
-                if(!found) head=n1,found=true;
-                n1->next=NULL;
-                n1=p;
-                p=rev(p);
-                if(pp)
-                    pp->next=p;
-                pp=n1;
-                p=n2;
-                n1->next=n2;
-                n1=n2;
-                cout<<"1 ";
-            }
-            cout<<"2 ";
-        }
+       ListNode *rev(ListNode *head)
+{
+    if (head == NULL || head->next == NULL)
         return head;
+    ListNode *prev = head, *curr = head->next, *Next = curr->next;
+    while (Next)
+    {
+        curr->next = prev;
+        prev = curr;
+        curr = Next;
+        Next = Next->next;
     }
+    curr->next = prev;
+    head->next = NULL;
+    return curr;
+}
+ListNode *reverseKGroup(ListNode *head, int k)
+{
+    ListNode *prev = head, *Next = head, *pp = NULL;
+    bool found = false;
+    while (Next)
+    {
+        for (int i = 1; i < k; i++)
+            if (Next)
+                Next = Next->next;
+        if (Next)
+        {
+            ListNode *Next2 = Next->next;
+            if (!found)
+                head = Next, found = true;
+            Next->next = NULL;
+            Next = prev;
+            prev = rev(prev);
+            if (pp)
+                pp->next = prev;
+            pp = Next;
+            prev = Next2;
+            Next->next = Next2;
+            Next = Next2;
+        }
+    }
+    return head;
+}
 };
