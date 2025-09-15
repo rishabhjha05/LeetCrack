98c216efd296f88d44f5638c1b23c68d53c4fad9
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-15 11:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (24.57%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -3,18 +3,11 @@ public:
     int bitwiseComplement(int n) {
         if(n==0)
             return 1;
-        vector<int> vec,rev;
-        int j=0,num=0;
+        int i=0,num=0;
         while(n){
-            vec.push_back(n%2);
+            num+=!(n%2)*pow(2,i++);
             n/=2;
         }
-        for(int i=vec.size()-1;i>=0;i--)
-            rev.push_back(!vec[i]);
-        for(int ele:rev)
-            cout<<ele<<" ";
-        for(int i=rev.size()-1;i>=0;i--)
-            num+=rev[i]*pow(2,j++);
         return num;
     }
 };
