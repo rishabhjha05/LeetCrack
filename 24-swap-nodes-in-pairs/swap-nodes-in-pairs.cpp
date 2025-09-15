706df95b9c59eeb024a9d26ca994a6595a0b4351
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-15 13:15:45 +0530

    Time: 2 ms (1.17%) | Memory: 11.3 MB (20.31%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -10,16 +10,45 @@
  */
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
-        ListNode* pt=head;
-        while(pt && pt->next){
-            ListNode* left=pt;
-            ListNode* right=pt->next;
-            int temp=left->val;
-            left->val=right->val;
-            right->val=temp;
-            pt=pt->next->next;
+    ListNode* rev(ListNode *head){
+            if(head==NULL || head->next==NULL)
+                return head;
+            ListNode *prev=head,*curr=prev->next,*Next=curr->next;
+            while(Next){
+                curr->next=prev;
+                prev=curr;
+                curr=Next;
+                Next=Next->next;
+            }
+            head->next=NULL;
+            curr->next=prev;
+            return curr;
         }
+    ListNode* swapPairs(ListNode* head) {
+     ListNode *prev = head, *Next = head, *pp = NULL;
+    bool found = false;
+    while (Next)
+    {
+        for (int i = 1; i < 2; i++)
+            if (Next)
+                Next= Next->next;
+        if (Next)
+        {
+            ListNode *Next2 = Next->next;
+            if (!found) head = Next,found=true;
+            Next->next = NULL;
+            Next = prev;
+            prev = rev(prev);
+            if (pp)
+                pp->next = prev;
+            pp=Next;
+            prev = Next2;
+            Next->next = Next2;
+            Next = Next2;
+            cout<<"2 ";
+        }
+        cout<<"1 ";
+    }
         return head;
     }
 };
