25b46c065149312a3fd276f111aacad32aebee20
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-16 22:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (28.52%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        int n=prices.size(),i=n-1;
-        vector<int> nextS(n);
-        stack<int> st;
-        while(i>=0){
-            while(st.size()!=0 && st.top()>prices[i])
-                st.pop();
-            if(st.size()==0)
-                nextS[i]=0;
-            else
-                nextS[i]=st.top();
-            st.push(prices[i]);
-            i--;
-        }
-        i=0;
-        while(i<n){
-            prices[i]-=nextS[i];
-            i++;
-        }
-        return prices;
+        
+    int i = prices.size() - 1;
+    vector<int> discount(prices.size(), 0);
+    stack<int> st;
+    while (i >= 0)
+    {
+        while (st.size() != 0 && st.top() > prices[i])
+            st.pop();
+        if (st.size() != 0)
+            discount[i] = st.top();
+        st.push(prices[i]);
+        i--;
+    }
+    for(int i=0;i<prices.size();i++)
+        prices[i]-=discount[i];
+    return prices;
     }
 };
