aae527641c39f3480dbe9c602ff5e325dc097345
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-16 19:11:11 +0530

    Time: 446 ms (3.69%) | Memory: 7.7 MB (97.29%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        for(int i=0;i<100000;i++){
+            int sum=0;
+            while(n){
+                sum+=pow(n%10,2);
+                n/=10;
+            }
+            if(sum==1)
+                return true;
+            n=sum;
+        }
+        return false;
+    }
+};
