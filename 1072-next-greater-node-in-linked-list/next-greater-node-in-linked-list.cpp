58d6a190410e434e97616ad38060568b527fb0d7
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-16 20:51:38 +0530

    Time: 4 ms (74.72%) | Memory: 46.9 MB (32.70%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -10,44 +10,28 @@
  */
 class Solution {
 public:
-    ListNode *rev(ListNode* head){
-        if(head==NULL|| head->next==NULL)
-            return head;
-        ListNode *prev=head,*curr=head->next,*Next=curr->next;
-        while(Next){
-            curr->next=prev;
-            prev=curr;
-            curr=Next;
-            Next=Next->next;
-        }
-        curr->next=prev;
-        head->next=NULL;
-        return curr;
+    vector<int> nextLargerNodes(ListNode* head) {  
+    vector<int> list;
+    ListNode *temp = head;
+    int i, size = 0;
+    while (temp)
+    {
+        list.push_back(temp->val);
+        temp = temp->next;
+        size++;
     }
-    vector<int> nextLargerNodes(ListNode* head) {
-        int size=0;
-        head=rev(head);
-        ListNode *temp=head;
-        while(temp)
-        {
-            size++;
-            temp=temp->next;
-        }
-        vector<int> sol(size);
-        stack<int> st;
-        int i=size-1;
-        temp=head;
-        while(i>=0){
-            while(st.size()!=0 && st.top()<=temp->val)
-                st.pop();
-            if(st.size()==0)
-                sol[i]=0;
-            else
-                sol[i]=st.top();
-            st.push(temp->val);
-            temp=temp->next;
-            i--;
-        }
-        return sol;
+    vector<int> NextG(size, 0);
+    stack<int> st;
+    i=size-1;
+    while (i >= 0)
+    {
+        while (st.size() != 0 && st.top() <= list[i])
+            st.pop();
+        if (st.size() != 0)
+            NextG[i] = st.top();
+        st.push(list[i]);
+        i--;
+    }
+    return NextG;
     }
 };
