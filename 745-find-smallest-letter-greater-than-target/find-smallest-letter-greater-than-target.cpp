7671968e114fb3a6833d791a20ac240208ce94a2
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-22 00:20:30 +0530

    Time: 4 ms (0.67%) | Memory: 19.7 MB (61.32%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        // int i=0,j=letters.size()-1;
-        // char ans;
-        // while(i<=j){
-        //     int mid=(i+j)/2;
-        //     cout<<i<<" "<<j<<endl;
-        //     if(letters[mid]>target){
-        //         ans=letters[mid];
-        //         i=mid+1;
-        //         j=mid-1;
-        //     }
-        //     else if(letters[mid]<target)
-        //         i=mid+1;
-        //     else
-        //         j=mid-1;
-        // }
-        // return ans;
-        char ch=letters[0];
-        int n=letters.size(),i=1;
-        while(i<n && (int)ch<=(int)target)
-            ch=letters[i++];
-        if(letters[i-1]>target)
-            return letters[i-1];
-        return letters[0];
+        int i=0,j=letters.size()-1;
+        char ans;
+        while(i<=j){
+            int mid=(i+j)/2;
+            cout<<i<<" "<<j<<endl;
+            if(letters[mid]>target){
+                ans=letters[mid];
+                j=mid-1;
+            }
+            else
+                i=mid+1;
+        }
+        if(ans<=target)
+            return letters[0];
+        return ans;
+        // char ch=letters[0];
+        // int n=letters.size(),i=1;
+        // while(i<n && (int)ch<=(int)target)
+        //     ch=letters[i++];
+        // if(letters[i-1]>target)
+        //     return letters[i-1];
+        // return letters[0];
     }
 };
