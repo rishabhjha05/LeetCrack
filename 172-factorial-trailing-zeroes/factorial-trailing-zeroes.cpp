2aef3af6af6f8d5e76fab6495eb8ce5f0aebc025
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-28 20:16:05 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (85.83%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        long long noT=0,noF=0;
+        long long noF=0;
         for(int i=1;i<=n;i++)
         {
             int n=i;
-            while(n%2==0)
-                noT++,n/=2;
-            n=i;
             while(n%5==0)
                 noF++,n/=5;
         }
-        return min(noT,noF);
+        return noF;
     }
 };
