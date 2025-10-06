b6d9f4eae26e12fc7c48f55e0796e504c0f52c39
rishabhjha05 <rishabhjha012043@gmail.com>
2025-10-06 09:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (48.26%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -1,30 +1,10 @@
 class Solution {
 public:
     int sumOfMultiples(int n) {
-        if(n<3)
-            return 0;
-        unordered_map<int,int> freq;
-        int sum=0,i=3;
-        while(i<=n)
-        {
-            freq[i]++;
-            i+=3;
-        }
-        i=5;
-        while(i<=n)
-        {
-            freq[i]++;
-            i+=5;
-        }
-        i=7;
-        while(i<=n)
-        {
-            freq[i]++;
-            i+=7;
-        }
-        for(auto i:freq)
-            sum+=i.first;
+        int sum=0;
+        for(int i=1;i<=n;i++)
+            if(i%3==0||i%5==0||i%7==0)
+                sum+=i;
         return sum;
     }
-    //{3,6,9,12,15}
 };
