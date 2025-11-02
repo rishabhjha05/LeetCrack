005d6ae1d8c52383db485b85c76b357e4cabdb03
rishabhjha05 <rishabhjha012043@gmail.com>
2025-11-02 23:46:06 +0530

    Time: 2798 ms (5.05%) | Memory: 209.4 MB (17.89%) - LeetSync

---
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> block(m,vector<int> (n,0));
+        
+        for(vector<int> vec : guards)
+            block[vec[0]][vec[1]]=-1;
+        for(vector<int> vec : walls)
+            block[vec[0]][vec[1]]=1;
+        for(vector<int> vec : block){
+            for(int ele: vec)
+                cout<<ele<<" ";
+            cout<<endl;
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++){
+                if(block[i][j]==-1)
+                {
+                    int r=i-1,c=j;
+                    while(r>=0 && block[r][c]!=-1 && block[r][c]!=1){
+                        block[r][c]=2;
+                        r--;
+                    }
+                    r=i+1;
+                    while(r<m && block[r][c]!=-1 && block[r][c]!=1){
+                        block[r][c]=2;
+                        r++;
+                    }
+                    r=i,c=j-1;
+                    while(c>=0 && block[r][c]!=-1 && block[r][c]!=1){
+                        block[r][c]=2;
+                        c--;
+                    }
+                    c=j+1;
+                    while(c<n && block[r][c]!=-1 && block[r][c]!=1){
+                        block[r][c]=2;
+                        c++;
+                    }
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                if(block[i][j]==0)
+                    count++;
+        return count;
+        for(vector<int> vec : block){
+            for(int ele: vec)
+                cout<<ele<<" ";
+            cout<<endl;
+        }
+        return 0;
+    }
+};
