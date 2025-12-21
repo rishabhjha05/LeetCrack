2e43c43fdc31987bac673ad3b4ec23104c774f48
rishabhjha05 <rishabhjha012043@gmail.com>
2025-12-21 19:51:54 +0530

    Time: 88 ms (33.33%) | Memory: 289.7 MB (8.33%) - LeetSync

--- 4160-maximum-sum-of-three-numbers-divisible-by-three/maximum-sum-of-three-numbers-divisible-by-three.cpp
+++ 4160-maximum-sum-of-three-numbers-divisible-by-three/maximum-sum-of-three-numbers-divisible-by-three.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     int maximumSum(vector<int>& nums) {
-        int ans=0,s1=0,s2=0,s3=0,s4=0;
-        unordered_map<int,vector<int>> md;
-        for(int ele : nums)
-            md[ele%3].push_back(ele);
-        sort(md[0].begin(),md[0].end());
-        sort(md[1].begin(),md[1].end());
-        sort(md[2].begin(),md[2].end());
-        int m=md[0].size(),n=md[1].size(),o=md[2].size();
-        if(m>=3){
-            s1+=md[0][m-1];
-            s1+=md[0][m-2];
-            s1+=md[0][m-3];
+         vector<int> l0, l1, l2;
+        
+        for (int x : nums) {
+            int p = x % 3;
+            if (p == 0) l0.push_back(x);
+            else if (p == 1) l1.push_back(x);
+            else l2.push_back(x);
         }
-        if(n>=3){
-            s2+=md[1][n-1];
-            s2+=md[1][n-2];
-            s2+=md[1][n-3];
-        }
-        if(o>=3){
-            s3+=md[2][o-1];
-            s3+=md[2][o-2];
-            s3+=md[2][o-3];
-        }
-        if(m>=1 && n>=1 && o>=1)
-        {
-            s4+=md[0][m-1];
-            s4+=md[1][n-1];
-            s4+=md[2][o-1];
-        }
-        ans=max(s1,s2);
-        ans=max(ans,s3);
-        ans=max(ans,s4);
+        
+        int ans = 0;
+        
+        sort(l0.begin(), l0.end(), greater<int>());
+        sort(l1.begin(), l1.end(), greater<int>());
+        sort(l2.begin(), l2.end(), greater<int>());
+        
+        if (l0.size() >= 3)
+            ans = max(ans, l0[0] + l0[1] + l0[2]);
+        
+        if (l1.size() >= 3)
+            ans = max(ans, l1[0] + l1[1] + l1[2]);
+        
+        if (l2.size() >= 3)
+            ans = max(ans, l2[0] + l2[1] + l2[2]);
+        
+        if (l0.size() >= 1 && l1.size() >= 1 && l2.size() >= 1)
+            ans = max(ans, l0[0] + l1[0] + l2[0]);
+        
         return ans;
     }
 };
