79c0bdedcf24d1db6b339c610b7563d594ef4862
rishabhjha05 <rishabhjha012043@gmail.com>
2025-12-23 13:33:17 +0530

    Time: 5 ms (14.60%) | Memory: 29.5 MB (49.02%) - LeetSync

--- 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.cpp
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
-        int n=nums.size(),ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                int len=j-i+1;
-                if(len>=l && len<=r && sum>0)
-                    ans=min(ans,sum);
+        int sum=0,ans=INT_MAX,n=nums.size();
+        for(int i=0;i<l&&i<n;i++){
+            sum+=nums[i];
+        }
+        cout<<sum;
+        for(int i=0;i<=n-l;i++){
+            int sm=sum,j=i+l;
+            while(j<n&&j-i+1<=r)
+            {
+                if(sm>0)
+                    ans=min(sm,ans);
+                sm+=nums[j];
+                j++;
             }
+            if(sm>0)
+                ans=min(sm,ans);
+            sum-=nums[i];
+            if(i!=n-l)
+                sum+=nums[i+l];
         }
         if(ans==INT_MAX)
             return -1;
