d9ef1ef80b0819f992cca3ddad242e2985aedcf2
rishabhjha05 <rishabhjha012043@gmail.com>
2025-12-25 15:04:51 +0530

    Time: 12 ms (93.89%) | Memory: 88.2 MB (94.19%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -13,12 +13,13 @@ public:
             long long mid=(low+high)/2,count=0;
             for(int ele : candies){
                 count+=ele/mid;
-            }
                 if(count>=k){
                     ans=mid;
                     low=mid+1;
+                    break;
                 }
-            else
+            }
+            if(count<k)
                 high=mid-1;
         }        
         return ans;
