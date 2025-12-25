d532bcc93ff3ea0a09a72a12aa9d3468f7d357e6
rishabhjha05 <rishabhjha012043@gmail.com>
2025-12-25 15:03:23 +0530

    Time: 4 ms (98.92%) | Memory: 88.3 MB (31.82%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        long long sum=0,ans=0;
+        for(int ele : candies)
+            sum+=ele;
+        if(sum<k)
+            return 0;
+        else if(sum==k)
+            return 1;
+        long long low=1,high=sum/k;
+        while(low<=high){
+            long long mid=(low+high)/2,count=0;
+            for(int ele : candies){
+                count+=ele/mid;
+                if(count>=k){
+                    ans=mid;
+                    low=mid+1;
+                    break;
+                }
+            }
+            if(count<k)
+                high=mid-1;
+        }        
+        return ans;
+    }
+};
