c01fbd18eae2ed6d46eae661bd836b82b429b238
rishabhjha05 <rishabhjha012043@gmail.com>
2025-12-26 14:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (24.61%) - LeetSync

--- 2083-three-divisors/three-divisors.cpp
+++ 2083-three-divisors/three-divisors.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    bool isPrime(int n){
-        if(n<=1)    
-            return false;
-        for(int i=2;i<=sqrt(n);i++)
-            if(n%i==0)
-                return false;
-        return true;
-    }
     bool isThree(int n) {
         bool flag=1;
         for(int i=2;i<n;i++){
