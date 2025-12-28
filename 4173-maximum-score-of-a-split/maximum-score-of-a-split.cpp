d85c7723d10720d1add5b41b2c839939feeb6b3c
rishabhjha05 <rishabhjha012043@gmail.com>
2025-12-28 21:31:48 +0530

    Time: 19 ms (20.00%) | Memory: 161.2 MB (40.00%) - LeetSync

--- 4173-maximum-score-of-a-split/maximum-score-of-a-split.cpp
+++ 4173-maximum-score-of-a-split/maximum-score-of-a-split.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     long long maximumScore(vector<int>& nums) {
         long long n=nums.size(),ans=LLONG_MIN,sum=0,mn=nums[n-1];
-        vector<long long> pre,suff(n-1);
-        for(int i=0;i<n-1;i++){
-            sum+=nums[i];
-            pre.push_back(sum);
-        }
+        vector<long long> pre(n-1),suff(n-1);
+        for(int ele:nums)
+            sum+=ele;
+        sum-=nums[n-1];
         for(int i=n-2;i>=0;i--){
             suff[i]=mn;
             mn=min((long long)nums[i],mn);
+            pre[i]=sum;
+            sum-=nums[i];
             ans=max(ans,(pre[i]-suff[i]));
         }
         return ans;
