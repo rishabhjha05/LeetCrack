6beedd18d36da4ab242748278e91b4708bf40631
rishabhjha05 <rishabhjha012043@gmail.com>
2025-12-29 00:12:35 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (88.11%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int i=0,neg=0,nn=0,pos=0,n=nums.size();
-        while(i<n && nums[i]<0)
-            i++;
-        neg=i;
-        while(i<n && nums[i]==0)
-            i++;
-        nn=i-neg;
-        pos=n-i;
+        int n=nums.size(),low=0,high=n-1,neg=0,pos=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]<0){
+                low=mid+1;
+                neg=mid+1;
+            }
+            else if(nums[mid]>0){
+                high=mid-1;
+                pos=n-mid;
+            }
+            else
+            {
+                neg=mid,pos=n-mid-1;
+                int i=mid-1;
+                while(i>=0 && nums[i]==0)
+                    i--,neg--;
+                i=mid+1;
+                while(i<n && nums[i]==0)
+                    i++,pos--;
+                return max(neg,pos);
+            }
+        }
         return max(neg,pos);
     }
 };
