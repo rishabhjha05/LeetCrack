0d4cb53fa990b57a6b57c13945ff1e17c037853c
rishabhjha05 <rishabhjha012043@gmail.com>
2026-01-21 23:42:03 +0530

    Time: 2901 ms (5.01%) | Memory: 73.1 MB (33.63%) - LeetSync

---
+++ 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool gcd(int n,int m){
+        int i=min(n,m);
+        while(i>1){
+            if(n%i==0 && m%i==0)
+                return 0;
+            i--;
+        }
+        return 1;
+    }
+    int countBeautifulPairs(vector<int>& nums) {
+        int n=nums.size(),count=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){  
+                int n=nums[i],m=nums[j]%10;
+                while(n/10!=0)
+                    n/=10;
+                if(gcd(n,m)){
+                    count++;
+                    cout<<n<<" "<<m<<"\n";
+                }
+            }
+        }
+        return count;
+    }
+};
