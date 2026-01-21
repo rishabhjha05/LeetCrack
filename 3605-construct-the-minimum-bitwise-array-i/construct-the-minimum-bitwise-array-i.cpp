a3ceadd19881228ee4382d1012bf1709dcd9936e
rishabhjha05 <rishabhjha012043@gmail.com>
2026-01-21 20:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (79.43%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -4,12 +4,18 @@ public:
         int n=nums.size();
         vector<int> ans(n,-1);
         for(int i=0;i<n;i++){
-            if(nums[i]%2!=0)
-            for(int j=0;j<=nums[i];j++){
-                if((j|(j+1))==nums[i]){
-                    ans[i]=j;
-                    break;
-                }}
+            if(nums[i]%2!=0){
+                int a=0,j=0,k=0,mask=1;
+                while(nums[i]&mask)
+                    mask<<=1,j++;
+                while(nums[i]){
+                    if(j!=1)
+                        a+=(nums[i]&1)*pow(2,k);
+                    k++,j--;
+                    nums[i]>>=1;
+                }
+                ans[i]=a;
+            }
         }
         return ans;
     }
