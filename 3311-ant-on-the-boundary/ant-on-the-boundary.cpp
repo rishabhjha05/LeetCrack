7e9d635a04bbc3df813152daa45ac6125fdea5cc
rishabhjha05 <rishabhjha012043@gmail.com>
2026-01-22 00:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (14.75%) - LeetSync

--- 3311-ant-on-the-boundary/ant-on-the-boundary.cpp
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int returnToBoundaryCount(vector<int>& nums) {
         int n=nums.size(),count=0;
-        for(int i=1;i<n;i++)
+        for(int i=1;i<n;i++){
             nums[i]+=nums[i-1];
-        for(int ele : nums)
-            if(ele==0)
+            if(nums[i]==0)
                 count++;
+        }
         return count;
-        
+
     }
 };
