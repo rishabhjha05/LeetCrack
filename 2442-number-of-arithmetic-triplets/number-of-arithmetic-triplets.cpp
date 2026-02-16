45dd861ca64d0b20fd8936af6f32241e0411072b
rishabhjha05 <rishabhjha012043@gmail.com>
2026-02-17 01:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (46.72%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
+    bool found(vector<int>& nums,int num,int lo,int hi){
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]==num)  
+                return 1;
+            else if(nums[mid]<num)
+                lo=mid+1;
+            else
+                hi=mid-1;
+        }
+        return 0;
+    }
     int arithmeticTriplets(vector<int>& nums, int diff) {
         int n=nums.size(),count=0;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
-                        count++;
-                }
+        for(int i=0;i<n-2;){
+            if(found(nums,diff+nums[i],i+1,n-1)){
+                if(found(nums,2*diff+nums[i],i+1,n-1))
+                    count++;
             }
+            i++;
+            while(i<n && nums[i]==nums[i-1])
+                i++;
         }
         return count;
     }
