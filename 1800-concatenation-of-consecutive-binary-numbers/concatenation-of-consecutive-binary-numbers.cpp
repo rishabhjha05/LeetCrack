367faeed3fdecb148a42b310edb3408c94d8e19d
rishabhjha05 <rishabhjha012043@gmail.com>
2026-03-01 19:24:49 +0530

    Time: 44 ms (58.67%) | Memory: 8 MB (89.33%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int concatenatedBinary(int n) {
-        long long ans=1,mod=1e9+7;
-        for(int i=2;i<=n;i++){
-            int count=log2(i)+1;
-            ans=(ans<<count)%mod;
+        long long ans=0,mod=1e9+7,d=0;
+        for(int i=1;i<=n;i++){
+            if((i&(i-1))==0)
+                d++;
+            ans=(ans<<d)%mod;
             ans=(ans+i)%mod;
         }
         return ans;
