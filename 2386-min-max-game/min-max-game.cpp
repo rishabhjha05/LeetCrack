d341071a3f7f1c77c3b2f913e9a5f4483ec77731
rishabhjha05 <rishabhjha012043@gmail.com>
2026-03-07 00:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (15.08%) - LeetSync

--- 2386-min-max-game/min-max-game.cpp
+++ 2386-min-max-game/min-max-game.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int minMaxGame(vector<int>& nums) {
-        while(nums.size()>1){
-            vector<int> vec;
-            int n=nums.size();
-            for(int i=0;i<n/2;i++){
-                if(i%2)
-                    vec.push_back(max(nums[2*i],nums[2*i+1]));
-                else
-                    vec.push_back(min(nums[2*i],nums[2*i+1]));
-            }
-            nums=vec;
+        queue<int> q;
+        bool mn=1;
+        int n=nums.size();
+        if(n==1)
+            return nums[0];
+        for(int i=0;i<n-1;i+=2){
+            if(mn)
+                q.push(min(nums[i],nums[i+1])),mn=0;
+            else
+                q.push(max(nums[i],nums[i+1])),mn=1;
         }
-        return nums[0];
+        while(q.size()>1){
+            int a=q.front();
+            q.pop();
+            int b=q.front();
+            q.pop();
+            if(mn)
+                q.push(min(a,b)),mn=0;
+            else
+                q.push(max(a,b)),mn=1;
+        }
+        return q.front();
     }
 };
