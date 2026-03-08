ac773aebcdc5583fab950751b7fe84bb59da7a42
rishabhjha05 <rishabhjha012043@gmail.com>
2026-03-08 15:58:25 +0530

    Time: 36 ms (7.55%) | Memory: 250 MB (5.02%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.cpp
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int smallestBalancedIndex(vector<int>& nums) {
-        long long sum=0,n=nums.size(),m=1;
-        vector<long long> pref,suff(n);
+        long long sum=0,n=nums.size(),m=1,i;
+        vector<long long> pref;
         for(int ele : nums){
             pref.push_back(sum);
             sum+=ele;
         }
-        for(int i=n-1;i>=0;i--){
+        vector<long long> suff(n,sum);
+        for(i=n-1;i>=0;i--){
             suff[i]=m;
             if(m>sum/nums[i])
-                m=sum;
+                break;
             else
                 m*=nums[i];
         }
+        if(i>=0)
+            suff[i]=m;
         for(int i=0;i<n;i++){
             if(pref[i]==suff[i])
                 return i;
