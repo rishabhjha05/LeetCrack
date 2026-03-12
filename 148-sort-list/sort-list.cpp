63864dbde961de9d0012efc1e01383ae307340e5
rishabhjha05 <rishabhjha012043@gmail.com>
2026-03-12 22:24:35 +0530

    Time: 19 ms (50.61%) | Memory: 58.2 MB (57.74%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,53 +10,17 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* list1,ListNode* list2){
-        ListNode* temp= new ListNode(-1);
-        ListNode* head=temp;
-        ListNode* temp1=list1,*temp2=list2;
-        while(temp1!=NULL && temp2!=NULL){
-            if(temp1->val>temp2->val){
-                //ListNode* x=new ListNode(temp2->val);
-                temp->next=temp2;
-                temp=temp2;
-                temp2=temp2->next;
-            }
-            else{
-                //ListNode* x=new ListNode(temp1->val);
-                temp->next=temp1;
-                temp=temp1;
-                temp1=temp1->next;
-            }
-        }
-        if(temp1==NULL)
-            temp->next=temp2;
-        else
-            temp->next=temp1;
-        return head->next;
-    }
-    void display(ListNode * head){
-        ListNode* temp=head;
-        while(temp!=NULL){
-            cout<<temp->val<<" ";
-            temp=temp->next;
-        }
-        cout<<endl;
-        return;
-    }
     ListNode* sortList(ListNode* head) {
-        if(head==NULL)
-            return NULL;
-        else if(head->next==NULL)
-            return head;
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        ListNode* temp=head;
+        priority_queue<int,vector<int>, greater<int>> q;
+        while(temp)
+            q.push(temp->val),temp=temp->next;
+        temp=head;
+        while(!q.empty()){
+            temp->val=q.top();
+            temp=temp->next;
+            q.pop();
         }
-        ListNode* head2=sortList(slow->next);
-        slow->next=NULL;
-        head=sortList(head);
-        return merge(head2,head);
+        return head;
     }
 };
