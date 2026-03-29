5c47a4fd5c99afdbaba57dfb95ef7a016085767f
rishabhjha05 <rishabhjha012043@gmail.com>
2026-03-29 21:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.13%) - LeetSync

--- 2401-count-asterisks/count-asterisks.cpp
+++ 2401-count-asterisks/count-asterisks.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int countAsterisks(string s) {
-        vector<int> vec;
-        int count=0,flag=1;
+        int count=0,flag=1,ans=0;
         for(char ch : s){
             if(ch=='|' && flag)
-                vec.push_back(count),count=0,flag=0;
+                ans+=count,count=0,flag=0;
             else if(ch=='|')
                 count=0,flag=1;
             else if(ch=='*')
                 count++;
         }
         if(flag && count)
-            vec.push_back(count);
-        int ans=0;
-        for(int ele : vec)
-            ans+=ele;
+            ans+=count;
         return ans;
     }
 };
