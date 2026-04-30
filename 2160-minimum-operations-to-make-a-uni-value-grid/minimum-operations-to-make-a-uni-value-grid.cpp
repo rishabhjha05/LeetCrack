308b9d993dbf37a688a54fafd479671d72395755
rishabhjha05 <rishabhjha012043@gmail.com>
2026-04-30 17:53:52 +0530

    Time: 1071 ms (5.04%) | Memory: 90 MB (60.84%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,48 +1,53 @@
 class Solution {
 public:
-    int partition(vector<int>& nums,int left,int right){
-        int p=nums[right],idx=0,i,j;
-        for(i=left;i<right;i++)
-            if(nums[i]<=p)
-                idx++;
-        idx+=left;
-        swap(nums[idx],nums[right]);
-        i=left,j=right;
-        while(i<idx && j>idx){
-            while(i<idx & nums[i]<=p)
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left;
+        
+        for (int j = left; j < right; j++) {
+            if (nums[j] <= pivot) {
+                swap(nums[i], nums[j]);
                 i++;
-            while(j>idx && nums[j]>p)
-                j--;
-            if(i<idx && j>idx)
-                swap(nums[i],nums[j]);
-        }
-        return idx;
-    }
-    int quickS(vector<int>& nums,int left,int right,int k){
-        if(left==right)
-            return nums[left];
-        int idx=partition(nums,left,right);
-        if(idx==k)
-            return nums[idx];
-        else if(idx<k)
-            return quickS(nums,idx+1,right,k);
-        else
-            return quickS(nums,left,idx-1,k);
-    }
-    int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> vec;
-        for(vector<int> v: grid){
-            for(int ele : v)
-                vec.push_back(ele);
-        }
-        int ans=0,n=vec.size(),y=quickS(vec,0,n-1,n/2);
-        for(vector<int> v: grid){
-            for(int ele : v){
-                if(abs(y-ele)%x!=0)
-                    return -1;
-                ans+=abs(y-ele)/x;
             }
         }
+        swap(nums[i], nums[right]);
+        return i;
+    }
+
+    int quickSelect(vector<int>& nums, int left, int right, int k) {
+        if (left == right) return nums[left];
+
+        int pivotIndex = partition(nums, left, right);
+
+        if (k == pivotIndex)
+            return nums[k];
+        else if (k < pivotIndex)
+            return quickSelect(nums, left, pivotIndex - 1, k);
+        else
+            return quickSelect(nums, pivotIndex + 1, right, k);
+    }
+
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> vec;
+
+        for (auto &row : grid) {
+            for (int ele : row) {
+                vec.push_back(ele);
+            }
+        }
+
+        int n = vec.size();
+        int median = quickSelect(vec, 0, n - 1, n / 2);
+
+        int ans = 0;
+        for (auto &row : grid) {
+            for (int ele : row) {
+                if (abs(median - ele) % x != 0)
+                    return -1;
+                ans += abs(median - ele) / x;
+            }
+        }
+
         return ans;
     }
 };
