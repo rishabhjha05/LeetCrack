25528ae8e1f6f30e549ebedbeff564fe9784815d
rishabhjha05 <rishabhjha012043@gmail.com>
2026-04-30 18:12:28 +0530

    Time: 89 ms (6.91%) | Memory: 15.8 MB (52.07%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,8 +1,36 @@
 class Solution {
 public:
+    int partition(vector<int>& nums,int left,int right){
+        int p=nums[right],idx=0,i,j;
+        for(i=left;i<right;i++)
+            if(nums[i]<=p)
+                idx++;
+        idx+=left;
+        swap(nums[idx],nums[right]);
+        i=left,j=right;
+        while(i<idx && j>idx){
+            while(i<idx & nums[i]<=p)
+                i++;
+            while(j>idx && nums[j]>p)
+                j--;
+            if(i<idx && j>idx)
+                swap(nums[i],nums[j]);
+        }
+        return idx;
+    }
+    int quickS(vector<int>& nums,int left,int right,int k){
+        if(left==right)
+            return nums[left];
+        int idx=partition(nums,left,right);
+        if(idx==k)
+            return nums[idx];
+        else if(idx<k)
+            return quickS(nums,idx+1,right,k);
+        else
+            return quickS(nums,left,idx-1,k);
+    }
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size(),mid=nums[n/2],ans=0;
+        int n=nums.size(), mid=quickS(nums,0,n-1,n/2),ans=0;
         for(int ele : nums)
             ans+=max(ele,mid)-min(ele,mid);
         return ans;
