d993d1c31690320d09ab2f4ee0b412b02268a72e
rishabhjha05 <rishabhjha012043@gmail.com>
2026-04-30 17:50:38 +0530

    Time: 291 ms (5.04%) | Memory: 108.2 MB (5.17%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,13 +1,43 @@
 class Solution {
 public:
+    int partition(vector<int>& nums,int left,int right){
+        int p=nums[right],idx=0,i,j;
+        for(i=left;i<right;i++)
+            if(nums[i]<=p)
+                idx++;
+        idx+=left;
+        swap(nums[idx],nums[right]);
+        i=left,j=right;
+        while(i<idx && j>idx){
+            while(i<idx & nums[i]<=p)
+                i++;
+            while(j>idx && nums[j]>p)
+                j--;
+            if(i<idx && j>idx)
+                swap(nums[i],nums[j]);
+        }
+        return idx;
+    }
+    int quickS(vector<int>& nums,int left,int right,int k){
+        if(left==right)
+            return nums[left];
+        int idx=partition(nums,left,right);
+        if(idx==k)
+            return nums[idx];
+        else if(idx<k)
+            return quickS(nums,idx+1,right,k);
+        else
+            return quickS(nums,left,idx-1,k);
+    }
     int minOperations(vector<vector<int>>& grid, int x) {
         vector<int> vec;
         for(vector<int> v: grid){
             for(int ele : v)
                 vec.push_back(ele);
         }
+        int ans=0,n=vec.size(),y=quickS(vec,0,n-1,n/2);
         sort(vec.begin(),vec.end());
-        int ans=0,n=vec.size(),y=vec[n/2];
+        cout<<y<<vec[n/2];
         for(vector<int> v: grid){
             for(int ele : v){
                 if(abs(y-ele)%x!=0)
