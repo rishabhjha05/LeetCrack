fe1cc8504ac2f44b6c46afb5429afafe303b7ebe
rishabhjha05 <rishabhjha012043@gmail.com>
2026-05-04 14:46:47 +0530

    Time: 19 ms (47.81%) | Memory: 100.9 MB (28.75%) - LeetSync

---
+++ 2777-find-the-distinct-difference-array/find-the-distinct-difference-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> distinctDifferenceArray(vector<int>& nums) {
+        int count=0,n=nums.size();
+        unordered_map<int,int> f1,f2;
+        vector<int> pre,suff(n),ans;
+        for(int ele : nums){
+            if(f1[ele]==0)
+                count++;
+            pre.push_back(count);
+            f1[ele]++;
+        }
+        count=0;
+        for(int i=n-1;i>=0;i--){
+            suff[i]=count;
+            if(f2[nums[i]]==0)
+                count++;
+            f2[nums[i]]++;
+        }
+        for(int i=0;i<n;i++)
+            ans.push_back(pre[i]-suff[i]);
+        return ans;
+    }
+};
