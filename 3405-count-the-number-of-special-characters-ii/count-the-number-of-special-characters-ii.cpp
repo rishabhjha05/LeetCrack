b09f4e75605d06bc1e6a37b01a8ce936bd7fb917
rishabhjha05 <rishabhjha012043@gmail.com>
2026-05-28 20:00:23 +0530

    Time: 90 ms (27.51%) | Memory: 25.4 MB (27.82%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        unordered_map<char,int> freq,pf,e;
-        int count=0,n=word.size();
-        string str="";
-        cout<<(char)('z'-32);
-        for(char ch: word)
-            freq[ch]++;
-        for(int i=n-1;i>=0;i--){
+        unordered_map<char,int> idx;
+        int n=word.size(),count=0;
+        for(int i=0;i<n;i++){
             char ch=word[i];
-            if(ch<=90 && pf['a'+(ch-'A')]==0 && freq['a'+(ch-'A')]!=0&&freq[ch]==1 && e[ch]==0)
-                count++,e[ch]++;
-            freq[ch]--,pf[ch]++;
+            if(ch<=90 && idx[ch]==0)
+                idx[ch]=i+1;
+            else if(ch>90)
+                idx[ch]=i+1;
+        }
+        for(auto itr: idx){
+            char ch=itr.first;
+            if(ch>90 && idx.count(ch-32) && itr.second<idx[ch-32])
+                count++;
         }
         return count;
     }
