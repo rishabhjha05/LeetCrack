b58ed4191aaa86def7c08ca342a9849aa8fbbca9
rishabhjha05 <rishabhjha012043@gmail.com>
2026-07-23 17:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (75.72%) - LeetSync

--- 4364-maximum-value-of-an-alternating-sequence/maximum-value-of-an-alternating-sequence.cpp
+++ 4364-maximum-value-of-an-alternating-sequence/maximum-value-of-an-alternating-sequence.cpp
@@ -4,9 +4,8 @@ public:
         if(n==1)
             return s;
         long long x=n-1,ans=s;
-        if(x%2==0)
-            return ans+(x/2)*(m-1)+1;
-        else 
-            return ans+(x/2)*(m-1)+m;
+        if(!(x%2))
+            x--;
+        return ans+(x/2)*(m-1)+m;
     }
 };
