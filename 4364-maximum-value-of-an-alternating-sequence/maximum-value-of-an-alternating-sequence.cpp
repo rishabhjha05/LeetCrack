9c44e43da8b9ff04fd7d089ee3ba550fd01df003
rishabhjha05 <rishabhjha012043@gmail.com>
2026-07-23 17:49:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (75.72%) - LeetSync

--- 4364-maximum-value-of-an-alternating-sequence/maximum-value-of-an-alternating-sequence.cpp
+++ 4364-maximum-value-of-an-alternating-sequence/maximum-value-of-an-alternating-sequence.cpp
@@ -3,9 +3,10 @@ public:
     long long maximumValue(int n, int s, int m) {
         if(n==1)
             return s;
-        long long x=n-1,ans=s;
-        if(!(x%2))
-            x--;
-        return ans+(x/2)*(m-1)+m;
+        long long ans=s;
+        n--;
+        if(!(n%2))
+            n--;
+        return ans+(n/2)*1LL*(m-1)+m;
     }
 };
