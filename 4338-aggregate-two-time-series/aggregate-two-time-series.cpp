f1df5466db6ec352155f1373549721f83230ca9d
rishabhjha05 <rishabhjha012043@gmail.com>
2026-07-26 11:06:10 +0530

    Time: 75 ms (82.13%) | Memory: 350 MB (95.59%) - LeetSync

---
+++ 4338-aggregate-two-time-series/aggregate-two-time-series.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& series1, vector<vector<int>>& series2) {
+        vector<vector<int>> ans;
+        int i=0,j=0,n=series1.size(),m=series2.size();
+        while(i<n && j<m){
+            if(series1[i][0]<series2[j][0])
+                ans.push_back({series1[i][0],series1[i][1]+series2[j][1]}),i++;
+            else if(series1[i][0]>series2[j][0])
+                ans.push_back({series2[j][0],series1[i][1]+series2[j][1]}),j++;
+            else
+                ans.push_back({series1[i][0],series1[i][1]+series2[j][1]}),i++,j++;
+        }
+        while(i<n)
+            ans.push_back({series1[i][0],series1[i][1]}),i++;
+        while(j<m)
+            ans.push_back({series2[j][0],series2[j][1]}),j++;
+        return ans;
+    }
+};
