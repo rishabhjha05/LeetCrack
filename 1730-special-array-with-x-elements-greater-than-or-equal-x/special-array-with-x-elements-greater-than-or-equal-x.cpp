8d1a4d60cd2560bcf9433b2cbe2b7538faa7a75f
rishabhjha05 <rishabhjha012043@gmail.com>
2026-07-26 17:07:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (65.14%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -4,10 +4,15 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<=n;i++){
-            int j=0;
-            while(j<n && nums[j]<i)
-                j++;
-            if(i==(n-j))
+            int l=0,h=n-1,idx=-1;
+            while(l<=h){
+                int mid=(l+h)/2;
+                if(nums[mid]<i)
+                    l=mid+1;
+                else if(nums[mid]>=i)
+                    idx=mid,h=mid-1;
+            }
+            if(i==(n-idx))
                 return i;
         }
         return -1;
