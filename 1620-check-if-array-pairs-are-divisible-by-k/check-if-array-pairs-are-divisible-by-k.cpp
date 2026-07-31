e7f14ef898f599fd12295da805c37af00189b675
rishabhjha05 <rishabhjha012043@gmail.com>
2026-07-31 11:05:49 +0530

    Time: 35 ms (61.92%) | Memory: 83.4 MB (40.57%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -2,13 +2,8 @@ class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         unordered_map<int,int> freq;
-        for(int ele : arr){
-            int rem=ele%k;
-            if(rem<0)
-                freq[rem+k]++;
-            else
-                freq[rem]++;
-        }
+        for(int ele : arr)
+                freq[(ele%k+k)%k]++;
         for(auto itr: freq){
             if(freq[itr.first]!=freq[(k-itr.first)%k] || (itr.first==k-itr.first && freq[itr.first]%2!=0))
                 return false;
