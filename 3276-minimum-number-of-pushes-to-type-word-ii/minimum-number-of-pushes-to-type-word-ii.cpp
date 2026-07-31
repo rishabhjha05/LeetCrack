ff28a4ca0f504a6b6f89823fa2d10ae5ac1e84d3
rishabhjha05 <rishabhjha012043@gmail.com>
2026-08-01 02:35:20 +0530

    Time: 28 ms (22.37%) | Memory: 26.7 MB (47.02%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        priority_queue<int> pq;
+        unordered_map<char,int> freq;
+        for(char ch: word)
+            freq[ch]++;
+        for(auto itr: freq)
+            pq.push(itr.second);
+        // for(auto itr:freq)
+        //     cout<<itr.first<<" : "<<itr.second<<endl;
+        long long count=1,m=1,ans=0;
+        while(!pq.empty()){
+            if(count==9)
+                count=1,m++;
+            ans+=m*pq.top();
+            count++;
+            pq.pop();
+        }
+        return ans;
+    }
+};
