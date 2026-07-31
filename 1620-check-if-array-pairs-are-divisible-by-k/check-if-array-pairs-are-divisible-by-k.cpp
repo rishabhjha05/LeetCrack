c1cc38aa023d5171d59c2cfa1a8167865fefa05a
rishabhjha05 <rishabhjha012043@gmail.com>
2026-07-31 10:43:14 +0530

    Time: 41 ms (53.02%) | Memory: 83.5 MB (27.58%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int,int> freq;
+        for(int ele : arr){
+            int rem=ele%k;
+            if(rem<0)
+                freq[rem+k]++;
+            else
+                freq[rem]++;
+        }
+        for(auto itr: freq){
+            if(freq[itr.first]!=freq[(k-itr.first)%k] || (itr.first==k-itr.first && freq[itr.first]%2!=0))
+                return false;
+            else 
+                freq[itr.first]=0,freq[(k-itr.first)%k]=0;
+        }
+
+        return true;
+    }
+};
