8bc28e03c9af355aa72096b42cb62d353bf7a1af
rishabhjha05 <rishabhjha012043@gmail.com>
2026-08-12 21:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (58.48%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -2,15 +2,6 @@ class Solution {
 public:
     unordered_map<int,bool> dp;
     bool divisorGame(int n) {
-        if(dp.count(n))
-            return dp[n];
-        for(int i=1;i<n;i++){
-            if(n%i==0){
-                if(!divisorGame(n-i))
-                    return dp[n]= true;
-            }
-        }
-        dp[n]= false;
-        return dp[n];
+        return n%2==0;
     }
 };
