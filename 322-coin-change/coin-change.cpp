33715f8678b776cdc60b559067019edc84dfc54d
rishabhjha05 <rishabhjha012043@gmail.com>
2026-08-13 12:35:27 +0530

    Time: 267 ms (5.01%) | Memory: 65.6 MB (5.05%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<int,int> dp;
+    int find(vector<int>& coins,int amt){
+        if(amt<0)
+            return -1;
+        else if(amt==0)
+            return 0;
+        else if(dp.count(amt))
+            return dp[amt];
+        int mn=INT_MAX;
+        for(int ele : coins){
+            int coin=find(coins,amt-ele);
+            if(coin!=-1)
+                mn=min(mn,1+coin);
+        }
+        if(mn==INT_MAX) 
+            dp[amt]=-1;
+        else
+            dp[amt]=mn;
+        return dp[amt];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        return find(coins,amount);
+    }
+};
